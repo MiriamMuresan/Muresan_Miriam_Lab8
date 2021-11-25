649ed2a30106b34a1b94fc5bc328ae8bbcd4de7e
Raluca Miriam Muresan <miriammuresan27@yahoo.ro>
2021-11-25 15:38:48 +0200

    Add project files.

---
+++ Data/Muresan_Miriam_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Muresan_Miriam_Lab8.Models;
+
+namespace Muresan_Miriam_Lab8.Data
+{
+    public class Muresan_Miriam_Lab8Context : DbContext
+    {
+        public Muresan_Miriam_Lab8Context (DbContextOptions<Muresan_Miriam_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Muresan_Miriam_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Muresan_Miriam_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Muresan_Miriam_Lab8.Models.Category> Category { get; set; }
+    }
+}
